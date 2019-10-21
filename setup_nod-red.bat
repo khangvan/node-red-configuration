bdb39735e693922be0f9c6f751b39234e4637a99
Khang Van <vanthaikhang@gmail.com>
2019-10-21 09:39:44 +0700

    start setup node-red on services window server

---
+++ setup_nod-red.bat
@@ -0,0 +1,6 @@
+mkdir c:\temp
+c:\nssm\nssm.exe install Node-RED "c:\Users\\"%USERNAME%"\AppData\Roaming\npm\node-red.cmd"
+c:\nssm\nssm.exe set Node-RED AppDirectory "c:\Users\\"%USERNAME%"\.node-red"
+c:\nssm\nssm.exe set Node-RED AppParameters "-u c:\Users\\"%USERNAME%"\.node-red > c:\temp\node-red.log"
+c:\nssm\nssm.exe set Node-RED Description "A wiring tool for the Internet of Things"
+pause
